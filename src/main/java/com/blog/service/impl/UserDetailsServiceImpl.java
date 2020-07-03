1c0699fbe1cffdc0f6171659fc4a165944d9fcfa
ZHONG86745 <407757361@QQ.COM>
2020-07-03 16:15:39 +0800

    zql

---
+++ src/main/java/com/blog/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.blog.service.impl;
+
+import com.blog.model.entity.User;
+import com.blog.service.IRoleService;
+import com.blog.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+/**
+ * UserDetailsService接口实现
+ * 钟
+ * 2020.7.3
+ */
+@Service
+public class UserDetailsServiceImpl {
+    @Autowired
+    private IUserService iUserService;//注入用户服务接口
+    @Autowired
+    private IRoleService iRoleService;//注入角色服务接口
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException{
+        User user =iUserService.findByLoginName(s);//根据登录名查用户信息
+        if(user!=null){
+            List<Role> list=iRoleService.findRoleByLoginUser(user);//根据用户查角色
+            StringBuffer roles=new StringBuffer();//用于保存角色名
+            for (Role r:list){
+                roles.append(r.getRole());
+            }
+            
+        }
+    }
+}
