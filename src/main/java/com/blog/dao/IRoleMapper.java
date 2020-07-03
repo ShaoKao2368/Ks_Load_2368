7e17cfd30d7a9d12c447a7d3c9adaa75261d9099
292945996 <2192945996@qq.com>
2020-07-03 11:44:13 +0800

    q11

---
+++ src/main/java/com/blog/dao/IRoleMapper.java
@@ -0,0 +1,20 @@
+package com.blog.dao;
+
+import com.blog.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+/*
+角色数据访问接口
+lwy
+ */
+@Mapper
+@Repository
+public interface IRoleMapper {
+    @Select("select r.* from t_role r,t_user u,t_user_role m where m.role_id=r.id=r.id and m.user_id=u.id and u.id=#{id}")
+    public List<Role> findRoleByLoginUser(User user);//根据用户名查询角色
+}
